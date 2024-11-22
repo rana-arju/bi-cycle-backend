1ddf16a8c49e089904283899e025aa80e4a199cf
rana-arju <arjurana20@gmail.com>
2024-11-22 21:45:07 +0600

    advance functionality add for order place

--- src/app/modules/orders/orders.model.ts
+++ src/app/modules/orders/orders.model.ts
@@ -1,31 +1,50 @@
 import { model, Schema } from 'mongoose';
 import { IOrder } from './orders.interface';
+import { Product } from '../products/products.model';
 
-const orderSchema = new Schema<IOrder>({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      'Please enter a valid email address',
-    ],
+const orderSchema = new Schema<IOrder>(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      trim: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        'Please enter a valid email address',
+      ],
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Product',
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required!"],
+      min: [1, 'Quantity must be at least 1'],
+    },
+    totalPrice: {
+      type: Number,
+      min: [0, 'Total price must be at least 0'],
+    },
   },
-  product: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'Product',
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [1, 'Quantity must be at least 1'],
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-    min: [0, 'Total price must be at least 0'],
-  },
-},{ timestamps: true });
+  { timestamps: true },
+);
 
+/*
+orderSchema.pre('save', async function (next) {
+  if (!this.isModified('quantity') && !this.isModified('product'))
+    return next();
+
+  try {
+    const product = await Product.findById(this.product);
+    if (product) {
+      this.totalPrice = product.price * this.quantity; // Calculate the total price
+    }
+    next();
+  } catch (error) {
+    next(error as any);
+  }
+});
+*/
 export const Order = model<IOrder>('Order', orderSchema);
--- src/app/modules/orders/orders.service.ts
+++ src/app/modules/orders/orders.service.ts
@@ -1,11 +1,37 @@
-import { IOrder } from "./orders.interface";
-import { Order } from "./orders.model";
-
+import { Product } from '../products/products.model';
+import { IOrder } from './orders.interface';
+import { Order } from './orders.model';
 
 //Place order
 
 const addOrderService = async (payload: IOrder): Promise<IOrder> => {
-  const result = await Order.create(payload);
+  const { email, product, quantity } = payload;
+
+  // find existing Product
+  const existingProduct = await Product.findById(product);
+  if (!existingProduct) {
+    throw new Error('Product not found');
+  }
+  if (existingProduct.quantity < quantity) {
+    throw new Error(
+      `Insufficient stock for product "${existingProduct.name}". Only ${existingProduct.quantity} items left.`,
+    );
+  }
+  existingProduct.quantity -= quantity;
+
+  if (existingProduct.quantity === 0) {
+    existingProduct.inStock = false;
+  }
+  await existingProduct.save();
+
+  const totalPrice = existingProduct.price * quantity;
+
+  const result = await Order.create({
+    email,
+    product,
+    quantity,
+    totalPrice,
+  });
   return result;
 };
 
@@ -14,7 +40,6 @@ const calculateRevenueService = async () => {
   const result = await Order.find();
   return result;
 };
-
 
 export const orderService = {
   addOrderService,
